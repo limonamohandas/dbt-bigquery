d4acd87074756d9f8f6d3eb106eaa3e88ecd16b8
limona.mohandas@datatonic.com <limona.mohandas@datatonic.com>
2021-11-17 11:58:28 +0000

    updated tests, docs and sources

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -6,7 +6,7 @@ with orders as (
         order_date,
         status
 
-    from datatonic-analytics-sandbox.jaffle_shop.orders
+    from {{ source('jaffle_shop', 'orders') }}
 
 )
 select * from orders
